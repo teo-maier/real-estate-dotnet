e86f7b87c59dbd1cb7e18afc4250740fb45eb017
Teo Maier <teo.maier@ontegrasolutions.com>
2023-01-09 10:11:52 +0200

    first commit - app works fine

--- ReastEstateWebApp/Program.cs
+++ ReastEstateWebApp/Program.cs
@@ -1,12 +1,24 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using ReastEstateWebApp.Data;
+using Microsoft.AspNetCore.Identity;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddDbContext<ReastEstateWebAppContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("ReastEstateWebAppContext") ?? throw new InvalidOperationException("Connection string 'ReastEstateWebAppContext' not found.")));
+    options.UseSqlServer(builder.Configuration.GetConnectionString("ReastEstateWebAppContext") ??
+                         throw new InvalidOperationException(
+                             "Connection string 'ReastEstateWebAppContext' not found.")));
+
+builder.Services.AddDbContext<LibraryIdentityContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("ReastEstateWebAppContext") ?? 
+                         throw new InvalidOperationException(
+                             "Connectionstring 'ReastEstateWebAppContext' not found.")));
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<LibraryIdentityContext>();
 
 var app = builder.Build();
 
@@ -22,6 +34,8 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
+app.UseAuthentication();
+;
 
 app.UseAuthorization();
 
